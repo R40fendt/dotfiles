1030cd2f86139c94b169acbc6c02429f4bac162b
R40fendt <r40test311@gmail.com>
2025-08-12 14:23:24 +0200

    Waybar und Hyprland, apt-install.sh entfernt

--- apt-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing dependencies..."
-sudo apt install hyprland nautilus zsh xdg-desktop-portal-hyprland swaybg waybar wofi kitty blueman-applet fzf batcat mdcat -y
-
-sudo snap install opera
-
-curl -sS https://starship.rs/install.sh | sh
